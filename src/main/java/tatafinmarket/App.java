5169ab08e155ac1b8f0d7343bcf3e3ef89b93337
naresh lanka <naresh.29devops@gmail.com>
2022-01-10 12:45:58 +0530

    new files for tatafinmarket

---
+++ src/main/java/tatafinmarket/App.java
@@ -0,0 +1,13 @@
+package tatafinmarket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
